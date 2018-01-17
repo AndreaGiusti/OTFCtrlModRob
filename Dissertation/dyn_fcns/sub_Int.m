3720708719134f2a4c96ff48eb7998b0b9058963
Andrea Giusti <giusti@in.tum.de>
2018-01-17 23:07:59 +0100

    update dissertation

--- Dissertation/dyn_fcns/sub_Int.m
+++
@@ -1,9 +0,0 @@
-function [C_Inf, C_Sup] = sub_Int(A_Inf,A_Sup,B_Inf,B_Sup)
-% sub_Int performs the sum between the multidimensional interval A and B:
-% [A]-[B] = [C]
-
-C_Inf = A_Inf-B_Sup;
-C_Sup = A_Sup-B_Inf;
-
-end
-
--- Dissertation/dyn_fcns/sum_Int.m
+++
@@ -1,9 +0,0 @@
-function [C_Inf, C_Sup] = sum_Int(A_Inf,A_Sup,B_Inf,B_Sup)
-% sum_Int performs the sum between the multidimensional interval A and B:
-% [A]+[B] =[C]
-
-C_Inf = A_Inf+B_Inf;
-C_Sup = A_Sup+B_Sup;
-
-end
-
